17212a5d193f99d7d9bf7fec71f3639bf82e6b50
Ben <benherzog11235@gmail.com>
2018-12-19 13:09:06 +0200

    add distribution support to cli for key characters

--- src/main.rs
+++ src/main.rs
@@ -1601,7 +1601,7 @@ pub mod xorcism {
         use ::dist as _d;
         use std::collections::HashMap;
 
-        const DIST_BY_NAME:[(&str,&[(u8,_d::Prob)]);4] = [
+        pub const DIST_BY_NAME:[(&str,&[(u8,_d::Prob)]);4] = [
             ("shakespeare", &_d::known::SHAKESPEARE),
             ("base64", &_d::known::BASE64),
             ("hex", &_d::known::HEX),
@@ -1612,31 +1612,39 @@ pub mod xorcism {
 
     pub mod cli {
         use clap::{Arg,App,ArgMatches};
-        
+        use xorcism::dist::DIST_BY_NAME;
+
         pub fn args() -> ArgMatches<'static> { 
+            let dist_names: Vec<&str> = 
+                DIST_BY_NAME
+                .into_iter()
+                .cloned()
+                .map(|(n,_)| n)
+                .collect();
+
             App::new("XorCism")
             .version("0.1")
             .author("Ben Herzog <benherzog11235@gmail.com>")
             .about("Breaks vigenere-like ciphers")
                 .arg(Arg::with_name("input_file")
-                    .short("f")
-                    .long("input_file")
                     .value_name("INPUT_FILE")
                     .help("Sets the input file to use")
                     .required(true)
                     .index(1))
-                .arg(Arg::with_name("plaintext distribution")
-                    .short("ptdist")
-                    .long("plaintext distribution")
+                .arg(Arg::with_name("plaintext_distribution")
+                    .short("-ptd")
+                    .long("--plaintext-distribution")
                     .value_name("PT_DIST")
                     .help("Sets the assumed distribution of the plaintext characters")
+                    .possible_values(&dist_names)
                     .default_value("shakespeare")
                 )
-                .arg(Arg::with_name("key distribution")
-                    .short("kdist")
-                    .long("key character distribution")
+                .arg(Arg::with_name("key_distribution")
+                    .short("-kd")
+                    .long("--key-distribution")
                     .value_name("KEY_DIST")
                     .help("Sets the assumed distribution of the key characters")
+                    .possible_values(&dist_names)
                     .default_value("uniform")
                 )
             .get_matches()
