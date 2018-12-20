0d758d491a5a526ac304746cbd77b022362d2c47
Ben <benherzog11235@gmail.com>
2018-12-20 12:59:34 +0200

    complete iterative guessing module, but introduce performance regression

--- src/main.rs
+++ src/main.rs
@@ -15,30 +15,68 @@ mod tests;
 
 use crypto::vigenere;
 use std::fs::File;
-use std::io::{Read,Write};
+use std::io::{self,Read,Write};
 use cli::GetArg;
+use itertools::Itertools;
 
 fn main() -> Result<(),String> {
     let args = cli::args();
         
     let ciphertext = 
         File::open(args.get("input_file")?)
-        .map_err(|e| format!("Could not open input file: {}",e))?
+        .map_err(|e| format!("Failed to open input file: {}",e))?
         .bytes()
         .collect::<Result<Vec<u8>,std::io::Error>>()
-        .map_err(|e| format!("Could not read input file: {}",e))?;
+        .map_err(|e| format!("Failed to read input file: {}",e))?;
         
     let pt_dist =  builtin::dist::by_name(args.get("plaintext_distribution")?)?;
     let key_dist = builtin::dist::by_name(args.get("key_distribution")?)?;
     let comb_func = builtin::comb::by_name(args.get("combination_function")?)?;
 
+    let mut chosen_solution: Result<Vec<u8>,&'static str> = 
+        Err("None of the proposed solutions were accepted.");
+    let mut cmd: String = String::new();
+
     let solutions = 
         vigenere::full_break(&ciphertext, &pt_dist, &key_dist, &comb_func)
-        .map_err(|e| format!("Break attempt failed: {}", e))?;
+        .map_err(|e| format!("Failed to break ciphertext: {}", e))?
+        .dedup();
+
+    let stdin = io::stdin();
+    'choose_solution: for solution in solutions {
+        let proposed_solution = 
+            solution
+            .map_err(|e| 
+                format!("Failed to compute possible solution: {}",e)
+            )?;
+
+        println!("Proposed solution (peek):");
+        { 
+            let peek = String::from_utf8_lossy(&proposed_solution[..100]);
+            println!("{}", peek);
+        }
+
+        'get_cmd: loop {
+            cmd.clear();
+            println!("(a)ccept or try (n)ext solution?");
+            stdin.read_line(&mut cmd);
+            match cmd.as_str().trim() {
+                "a" => {
+                    chosen_solution = Ok(proposed_solution);
+                    break 'choose_solution;
+                }, "n" => {
+                    continue 'choose_solution;
+                }, _ => {
+                    println!("Invalid command.");
+                    continue 'get_cmd;
+                }
+            }
+        }
+    }
     
     File::create(args.get("output_file")?)
-    .map_err(|e| format!("Could not create output file: {}", e))?
-    .write_all(&solutions.clone().next().unwrap().unwrap())
+    .map_err(|e| format!("Failed to create output file: {}", e))?
+    .write_all(&chosen_solution?)
     .map_err(|e| format!("Failed to write to output file: {}",e))?;
 
     Ok(())
